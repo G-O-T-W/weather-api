fa33e04a65ec0747ab0b90dcc09c83377a0892eb
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-10-25 00:50:29 +0530

    Add URL endpoint for yearly and station wise data

--- main.py
+++ main.py
@@ -10,14 +10,31 @@ def home():
                            sta_names=stations.to_html())
 
 
-@app.route("/api/v1/<station>/<date>")
+@app.route("/api/v1/<station>/<date>/")
 def get_temperature(station, date):
     filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    df =  pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>/")
+def get_sta_data(station):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df =  pd.read_csv(filename, skiprows=20, parse_dates=['    DATE']).head(100)
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/annual/<station>/<year>/")
+def get_yearly_data(station, year):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df =  pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(year)]
+    return result.to_dict(orient="records")
 
 
 if __name__ == "__main__":
